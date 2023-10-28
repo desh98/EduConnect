21a95901848df93e448f0b5700a69450e96954a3
desh98 <deshaniabey826@gmail.com>
2023-10-27 23:01:26 -0700

    Dashboard => Menu

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,23 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Leon Dashboard',
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   State<HomePage> createState() => _HomePageState();
@@ -22,9 +37,66 @@ class _HomePageState extends State<HomePage> {
         actions: [
           // sign out button
           IconButton(
-            onPressed: () {},
+            onPressed:
+                signOut, // Call the signOut function when the button is pressed
             icon: Icon(Icons.logout),
           )
+        ],
+      ),
+      body: Row(
+        children: <Widget>[
+          // Sidebar Menu (you can place your sidebar menu code here)
+          Container(
+            width: 250,
+            color: Colors.blueGrey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                ListTile(
+                  title: Text(
+                    'Classes',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  leading: Icon(Icons.school, color: Colors.white),
+                  onTap: () {
+                    // Navigate to Courses Screen
+                  },
+                ),
+                ListTile(
+                  title: Text(
+                    'Announcements',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  leading: Icon(Icons.notifications, color: Colors.white),
+                  onTap: () {
+                    // Navigate to Announcements Screen
+                  },
+                ),
+                ListTile(
+                  title: Text(
+                    'Profile',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  leading: Icon(Icons.person, color: Colors.white),
+                  onTap: () {
+                    // Navigate to Profile Screen
+                  },
+                ),
+              ],
+            ),
+          ),
+          // Main Content
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(20),
+              child: Center(
+                child: Text(
+                  'Welcome to Leon Dashboard!',
+                  style: TextStyle(fontSize: 24),
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
